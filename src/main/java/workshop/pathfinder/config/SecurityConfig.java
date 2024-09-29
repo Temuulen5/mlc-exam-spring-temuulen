6b841ef59c5217c4a7ec8be9e3ee0e46292ac076
T.Temuulen <tumursukhtemuulen5@gmail.com>
2024-09-29 23:21:39 +0800

    glitch after login request

--- src/main/java/workshop/pathfinder/config/SecurityConfig.java
+++ src/main/java/workshop/pathfinder/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package workshop.pathfinder.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -50,9 +51,13 @@ public class SecurityConfig {
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService(userRepository));
+        return authenticationManagerBuilder.build();
+    }//new
+
 
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepository) {
